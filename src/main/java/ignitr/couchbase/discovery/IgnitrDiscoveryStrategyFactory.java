ea6681f157e2bddbab087a6d47714d00c4ed87a8
gregwhitaker <gwhitake@gmail.com>
2015-06-07 16:54:43 -0700

    Added test scaffolding

---
+++ src/main/java/ignitr/couchbase/discovery/IgnitrDiscoveryStrategyFactory.java
@@ -0,0 +1,12 @@
+package ignitr.couchbase.discovery;
+
+import com.netflix.governator.annotations.AutoBindSingleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AutoBindSingleton
+public class IgnitrDiscoveryStrategyFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IgnitrDiscoveryStrategyFactory.class);
+
+    
+}
